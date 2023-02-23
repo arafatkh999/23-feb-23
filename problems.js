9d7c9bba9c3f2decbf59cfc0618d30bce151b6e9
arafatkh999 <arafatkh999@gmail.com>
2023-02-23 18:28:14 +0600

    arrow function

---
+++ problems.js
@@ -0,0 +1,74 @@
+//Problem-01
+//  const myvar = 'arafat';
+// console.log(`${myvar}`);
+
+// const student ={
+//     name:'arafat',
+//     age:25,
+//     isMale:true,
+//     job:'bekar'
+// };
+// const myName = student.name;
+// const {name}= student;
+// const myage = student.age;
+// const {age} =student;
+
+// console.log(`${myName} ${myage}`);
+// console.log(name, age)
+
+
+// Problem-02
+// const output = n => n/5;
+// console.log(output(55));
+
+
+// Problem-03
+// const output1 = (x,y) => (x+2)*(y+2);
+// console.log(output1(3,5));
+
+
+// Problem-04
+// const output2 =(x,y,z) =>x*y*z;
+// console.log(output2(2,3,4));
+
+
+// Problem-05
+// const output3 =(x,y) =>{
+//     return (x+2)*(y+2);
+// }
+// console.log(output3(5,7));
+
+
+// problem-06
+// const arr =[1,2,3,4,5,6,7,8,9];
+// const result = arr.map(n => n*5);
+// console.log(result);
+
+
+// problem-07
+// const arr1 =[1,2,3,4,5,6,7,8,9,11,12,13,14,15];
+// const result1 = arr1.filter(n => n%2 === 1);
+// console.log(result1);
+
+
+// Problem-08
+// const arr2 =[
+//     {name:'mobile', price:1200},
+//     {name:'laptop', price:10000},
+//     {name:'tablet', price:5000},
+//     {name:'bike', price:30000}
+// ];
+
+// const result2 = arr2.find(n => n.price ===5000)
+// console.log(result2);
+
+
+// problem-09
+// const arr3 =[1,2,3,4,5,6,7,8,9];
+// const [one,two,three]=arr3;
+// console.log(three);
+
+
+// Problem-10
+// const result3 =(x,y,z=10)=>x+y+z;
+// console.log(result3(5,7));
